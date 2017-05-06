5408b98083a4a8929a1a454ba7dc40405f2ca909
Steven Stevanus <stvn.org@gmail.com>
2017-05-06 23:15:26 +0800

    New node

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req,res) { res.send("Hello World"); });
+app.get('/', function(req,res) { res.send("Hello World1"); });
 
 app.listen(process.env.PORT || 5000);
 
